581299f96209d2eef14ca6c2bd28b02dafd5ecce
rohan-repos <rohannagendra36@gmail.com>
2020-09-21 20:55:04 -0400

    update JS exercise 3

---
+++ JS_exercises/exercise-3/index.js
@@ -0,0 +1,19 @@
+console.log("Age above declaration: "+randomValue+" Type: "+typeof(randomValue));
+var randomValue=2342;
+console.log("Age after declaration: "+randomValue+" Type: "+typeof(randomValue));
+let age=30
+let firstName="Anonymous"
+let courseCompleted=false
+let finalScore=null;
+let winningNumbers;
+console.log("Age: "+age+" Type: "+typeof(age));
+strNum = String(age);//string to number without declaring variable sage
+console.log("StrNum: "+strNum+" Type: "+typeof(strNum));
+let strNumSecond= "20"/"10"; //String to number
+console.log("strNumSecond: "+strNumSecond+" Type: "+typeof(strNumSecond));
+let strNumThird = "20"/10; //String to number
+console.log("strNumThird: "+strNumThird+" Type: "+typeof(strNumThird));
+console.log("First Name: "+firstName+" Type: "+typeof(firstName));
+console.log("courseCompleted: "+courseCompleted+" Type: "+typeof(courseCompleted));
+console.log("Final Score: "+finalScore+" Type: "+typeof(finalScore));
+console.log("Winning Numbers; "+winningNumbers+" Type: "+typeof(winningNumbers));
