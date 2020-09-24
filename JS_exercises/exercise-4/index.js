5952041f02ce5d9204c5573240c7f3c322dbb901
rohan-repos <rohannagendra36@gmail.com>
2020-09-23 21:36:36 -0400

    updated JS 4

--- JS_exercises/exercise-4/index.js
+++ JS_exercises/exercise-4/index.js
@@ -3,14 +3,4 @@ let numOne = prompt("Enter the first number to compare.");
 let numTwo = prompt("Enter the second number to compare");
 console.log("numOne: "+typeof(numOne));
 console.log("numTwo: "+typeof(numTwo));
-if(numOne > numTwo)
-{
-    confirm("Number 1 is greater");
-}
-else if(numTwo > numOne){
-    confirm("Number 2 is greater");
-}
-let res="A";
-console.log(res.charCodeAt(0)); //string to unicode
-console.log(String.fromCharCode(65));//unicode to string
-
+console.log(confirm(numOne>numTwo));
