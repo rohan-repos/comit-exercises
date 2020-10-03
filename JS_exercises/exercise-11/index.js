2bc90310e2a7836bbd662bb9db91ca48a0482d57
rohan-repos <rohannagendra36@gmail.com>
2020-10-02 21:03:51 -0400

    Updated JS exercise 11 (Lesson 28)

--- JS_exercises/exercise-11/index.js
+++ JS_exercises/exercise-11/index.js
@@ -1,3 +1,30 @@
+"user strict";
 let user={};
 console.log(user);
-console.log("type of"+typeof(user));
+console.log("type is: "+typeof(user));
+user.username="batman";
+user.password="hunter2";
+console.log(user);
+let usrName=prompt("Enter username");
+let usrPass=prompt("Enter password");
+if(usrName==user.username && usrPass==user.password)
+{
+    console.log("Access Granted");
+}
+else
+{
+    console.log("Access Denied");
+}
+
+let anotherUser={username:"wonderwoman",
+                 password:123123,
+                greet(){
+                    console.log("Hello I am "+this.username);
+                },
+                setUsername(user){
+                    this.username=user;
+                }};
+
+anotherUser.greet();
+anotherUser.setUsername("Superman");
+anotherUser.greet();
